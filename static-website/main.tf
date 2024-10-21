d4b3d56b61672a2e7ebdb04680021dbbdc809559
marvinlanhenke <lanhenke@googlemail.com>
2024-10-21 12:15:21 +0200

    chore: change block public access

--- static-website/main.tf
+++ static-website/main.tf
@@ -9,11 +9,10 @@ resource "aws_s3_bucket" "static_site" {
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.static_site.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
-
 }
 
 resource "aws_s3_bucket_policy" "public_read" {
